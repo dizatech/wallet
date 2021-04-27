661e98019bcce33aee3525c48b19ec873e9fadb0
foruzanAmiri <foruzan.amiridavani@gmail.com>
2021-04-27 17:17:03 +0430

    route is updated

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group([
-    'namespace' => 'Wallet\Http\Controllers',
+    'namespace' => 'Modules\Wallet\Http\Controllers',
     'prefix'    => 'panel',
     'middleware'=> ['web', 'auth', 'verified']
 ],function () {
@@ -11,7 +11,7 @@ Route::group([
     Route::resource('transaction', 'UserWalletTransactionController');
 });
 Route::group([
-    'namespace' => 'Wallet\Http\Controllers',
+    'namespace' => 'Modules\Wallet\Http\Controllers',
     'prefix' => 'account',
     'middleware' => ['web', 'auth', 'verified']
 ], function (){
