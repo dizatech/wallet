7f91150b911a3994775b8863ad600821b1817b53
Omid Reza Vasheghani Farahani <omidrezav@gmail.com>
2022-09-14 13:02:59 +0430

    Added deposit method in UserWalletRepository

--- src/Facades/UserWalletFacade.php
+++ src/Facades/UserWalletFacade.php
@@ -9,6 +9,7 @@ namespace Modules\Wallet\Facades;
  * @method static array allWithPaginate()
  * @method static array getActive()
  * @method static Modules\Wallet\Models\UserWalletTansaction withdraw(Modules\Wallet\Models\UserWallet $user_wallet, int $amount, string $description='')
+ * @method static Modules\Wallet\Models\UserWalletTansaction deposit(Modules\Wallet\Models\UserWallet $user_wallet, int $amount, int $wallet_id, int $user_id, string $description='')
  *
  *
  * @see \Modules\Wallet\Repositories\UserWalletRepository
--- src/Repositories/UserWalletRepository.php
+++ src/Repositories/UserWalletRepository.php
@@ -4,6 +4,7 @@
 namespace Modules\Wallet\Repositories;
 
 use Illuminate\Support\Facades\Auth;
+use Modules\Wallet\Models\UserWallet;
 use Modules\Wallet\Models\UserWalletTransaction;
 use Modules\Wallet\Models\Wallet;
 
@@ -18,32 +19,57 @@ class UserWalletRepository
     {
         return UserWalletTransaction::query()->paginate();
     }
+
     public function getActive()
     {
         return Wallet::where('is_active', 1)->get();
     }
 
-    public function findActiveWalletByTitle($search) {
+    public function findActiveWalletByTitle($search)
+    {
         return Wallet::query()
             ->where(function ($query) use ($search) {
-                $query->where('is_active', 1)->where('title', 'LIKE', ['%'. $search . '%']);
+                $query->where('is_active', 1)->where('title', 'LIKE', ['%' . $search . '%']);
             })->get();
     }
 
-    public function withdraw($user_wallet, $amount, $description='')
+    public function withdraw($user_wallet, $amount, $description = '')
     {
-        if( $amount > $user_wallet->balance ){
+        if ($amount > $user_wallet->balance) {
             return FALSE;
         }
 
         $transaction = new UserWalletTransaction();
         $transaction->fill([
-            'description'   => $description,
-            'creator_id'    => Auth::id(),
-            'wallet_id'     => $user_wallet->wallet_id,
-            'user_id'       => $user_wallet->user_id,
+            'description' => $description,
+            'creator_id' => Auth::id(),
+            'wallet_id' => $user_wallet->wallet_id,
+            'user_id' => $user_wallet->user_id,
         ]);
         $transaction->amount = -1 * $amount;
+        $transaction->save();
+
+        return $transaction;
+    }
+
+    public function deposit($user_wallet, $amount, $wallet_id, $user_id,  $description = '')
+    {
+        if (!isset($user_wallet)) {
+            $user_wallet = new UserWallet();
+            $user_wallet->user_id = $user_id;
+            $user_wallet->wallet_id = $wallet_id;
+            $user_wallet->balance = $amount;
+            $user_wallet->save();
+        }
+
+        $transaction = new UserWalletTransaction();
+        $transaction->fill([
+            'description' => $description,
+            'creator_id' => Auth::id(),
+            'wallet_id' => $wallet_id,
+            'user_id' => $user_id,
+        ]);
+        $transaction->amount = $amount;
         $transaction->save();
 
         return $transaction;
